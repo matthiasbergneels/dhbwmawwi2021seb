881180d68ca5fa14dce958ad3991074f72539d40
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2022-06-10 16:34:16 +0200

    FlowLayout Example

---
+++ src/lecture/chapter10/FlowLayoutExample.java
@@ -0,0 +1,26 @@
+package lecture.chapter10;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutExample extends JFrame {
+
+  private FlowLayoutExample(){
+    this.setTitle("Flow Layout Example");
+
+    this.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+    this.add(new JLabel("Hallo WWI2021SEB"));
+    this.add(new JTextField("Pausen Dauer"));
+    this.add(new JButton("Starte Pause"));
+
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.pack();
+    this.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    new FlowLayoutExample();
+  }
+
+}
