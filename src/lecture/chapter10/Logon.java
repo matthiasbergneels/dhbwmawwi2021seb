8a76bb1180333e00056cf1cb8232a15d71297335
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2022-06-15 16:22:31 +0200

    Logon UI Example

---
+++ src/lecture/chapter10/Logon.java
@@ -0,0 +1,117 @@
+package lecture.chapter10;
+
+import java.awt.*;
+import java.text.ParseException;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.text.MaskFormatter;
+
+public class Logon extends JFrame{
+
+  public Logon() throws ParseException{
+    this.setTitle("Logon");
+
+    final String[] PROTOCOL_VALUE_HELP = {"FTP", "Telnet", "SMTP", "HTTP"};
+    JComboBox<String> myComboBox = new JComboBox<>(PROTOCOL_VALUE_HELP);
+
+    JFormattedTextField portField = new JFormattedTextField(new MaskFormatter("#####"));
+    portField.setColumns(3);
+
+    // initialize Panels
+    JPanel mainPanel = new JPanel(new BorderLayout());
+
+    JPanel southPanel = new JPanel(new FlowLayout());
+    JPanel centerPanel = new JPanel(new FlowLayout());
+
+    JPanel connectionPanel = new JPanel(new GridLayout(0, 2));
+    JPanel filePanel = new JPanel(new GridLayout(0, 2));
+
+    FlowLayout cellFlowLayout = new FlowLayout(FlowLayout.LEFT);
+
+    //create & assign elements for connection area
+    JPanel flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JLabel("User:"));
+    connectionPanel.add(flowLayoutForCell);
+    //flowLayoutForCell = new JPanel(cellFlowLayout);
+    //flowLayoutForCell.add(new JTextField(3));
+    //connectionPanel.add(flowLayoutForCell);
+    connectionPanel.add(new JTextField(3)); // --> kein extra JPanel mit FlowLayout
+
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JLabel("Passwort:"));
+    connectionPanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JPasswordField(5));
+    connectionPanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JLabel("Art:"));
+    connectionPanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(myComboBox);
+    connectionPanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JLabel("Host:"));
+    connectionPanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    JTextField host = new JTextField(5);
+    flowLayoutForCell.add(host);
+    connectionPanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JLabel("Port:"));
+    connectionPanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(portField);
+    connectionPanel.add(flowLayoutForCell);
+
+    // create & add Fields for File Area
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JLabel("Quelle:"));
+    filePanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JTextField(10));
+    filePanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JLabel("Ziel:"));
+    filePanel.add(flowLayoutForCell);
+    flowLayoutForCell = new JPanel(cellFlowLayout);
+    flowLayoutForCell.add(new JTextField(10));
+    filePanel.add(flowLayoutForCell);
+
+    // create & assign Buttons
+    JButton okButton = new JButton("Ausgeben");
+    JButton cancelButton = new JButton("Schliessen");
+
+    southPanel.add(okButton);
+    southPanel.add(cancelButton);
+
+    // create & assign Borders
+    Border etchedBorder = BorderFactory.createEtchedBorder();
+    Border connectionBorder = BorderFactory.createTitledBorder(etchedBorder, "Verbindung");
+    Border fileBorder = BorderFactory.createTitledBorder(etchedBorder, "Datei");
+    Border centerBorder = BorderFactory.createBevelBorder(BevelBorder.LOWERED);
+
+    connectionPanel.setBorder(connectionBorder);
+    filePanel.setBorder(fileBorder);
+    centerPanel.setBorder(centerBorder);
+
+    // combine Panels
+    centerPanel.add(connectionPanel);
+    centerPanel.add(filePanel);
+
+    mainPanel.add(centerPanel, BorderLayout.CENTER);
+    mainPanel.add(southPanel, BorderLayout.SOUTH);
+
+    this.add(mainPanel);
+
+    // set JFrame behavior
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.pack();
+    this.setVisible(true);
+  }
+
+  public static void main(String[] args) throws ParseException {
+    new Logon();
+  }
+}
