4d5c97c142b01773d3f0f6f06702b51ace222a4b
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2022-07-01 16:55:04 +0200

    LinkedList completion

--- src/lecture/chapter12/LinkedList.java
+++ src/lecture/chapter12/LinkedList.java
@@ -3,40 +3,106 @@ package lecture.chapter12;
 public class LinkedList {
 
   private Node firstNode;
+  private Node lastNode;
+  private int size = 0;
 
   public void add(String word){
     Node newNode = new Node(word);
 
     if(firstNode == null){
       firstNode = newNode;
+      lastNode = newNode;
+      size++;
       return;
     }
 
+    /*
     Node currentNode = firstNode;
     while(currentNode.getNextNode() != null){
       currentNode = currentNode.getNextNode();
     }
 
     currentNode.setNextNode(newNode);
+     */
+
+    lastNode.setNextNode(newNode);
+    lastNode = newNode;
+    size++;
 
   }
 
   public int size(){
-    // TODO
-    return 0;
+    return size; // --> O(1)
+
+    /*
+    int count = 0;
+
+    if(firstNode == null){
+      return 0;
+    }
+
+    Node currentNode = firstNode;
+    count++;
+    while(currentNode.getNextNode() != null){
+      count++;
+      currentNode = currentNode.getNextNode();
+    }
+
+    return count;
+
+     */
   }
 
   public boolean contains(String word){
-    // TODO
+
+    Node currentNode = firstNode;
+    while(currentNode != null){
+      if(word.equals(currentNode.getWord())){
+        return true;
+      }
+      currentNode = currentNode.getNextNode();
+    }
+
     return false;
   }
 
   public void printList(){
-    // TODO
+    if(firstNode == null){
+      System.out.println("Die Liste ist leer");
+      return;
+    }
+
+    Node currentNode = firstNode;
+    while(currentNode.getNextNode() != null){
+      System.out.println(currentNode.getWord());
+      currentNode = currentNode.getNextNode();
+    }
+
+    System.out.println(currentNode.getWord());
+
   }
 
   public boolean remove(String word){
-    // TODO
+
+    if(firstNode != null){
+
+      if(firstNode.getWord().equals(word)){
+        firstNode = firstNode.getNextNode();
+        size--;
+        return true;
+      }
+
+      Node currentNode = firstNode;
+      while(currentNode.getNextNode() != null){
+        if(currentNode.getNextNode().getWord().equals(word)){
+          currentNode.setNextNode(currentNode.getNextNode().getNextNode());
+          size--;
+          return true;
+        }
+        currentNode = currentNode.getNextNode();
+      }
+    }
+
     return false;
   }
 
--- src/lecture/chapter12/LinkedListExample.java
+++ src/lecture/chapter12/LinkedListExample.java
@@ -9,6 +9,21 @@ public class LinkedListExample {
     wordList.add("Erstens");
     wordList.add("Zweitens");
     wordList.add("Drittens");
+    wordList.add("Drittens");
+    wordList.add("Klaus");
+    wordList.add("Drittens");
+    wordList.add("Drittens");
+    wordList.add("Drittens");
+    wordList.add("Fünftens");
+
+    System.out.println("Erstens :" + wordList.contains("Erstens"));       // Big-O: Best-Case --> O(1)
+    System.out.println("Klaus :" + wordList.contains("Klaus"));           // Big-O: Mid-Case --> O (5) --> Average-Case --> O(n/2)
+                                                                          // --> Wegfall von Konstanten O(n)
+    System.out.println("Fünftens :" + wordList.contains("Fünftens"));     // Big-O: Worst-Case --> O (n)
+    System.out.println("HansFranz :" + wordList.contains("HansFranz"));   // Big-O: Worst-Case --> O (n)
+
+    System.out.println("Anzahl Elemente in der Liste: " + wordList.size());
+    wordList.printList();
 
     System.out.println("Liste beendet");
 
